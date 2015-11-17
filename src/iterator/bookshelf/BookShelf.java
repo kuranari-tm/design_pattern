2c2150b1eb61d8bcc50c6c98628d6e49c37c6009
Tomohisa Kuranari <m.qureana@gmail.com>
2015-11-17 23:04:58 +0900

    BookShelfをArrayListを使って実装

--- src/iterator/bookshelf/BookShelf.java
+++ src/iterator/bookshelf/BookShelf.java
@@ -1,23 +1,23 @@
 package iterator.bookshelf;
 
+import java.util.ArrayList;
+
 import iterator.Aggregate;
 import iterator.Iterator;
 
 public class BookShelf implements Aggregate {
-	private Book[] books;
-	private int last = 0;
-	public BookShelf(int maxsize) {
-		this.books = new Book[maxsize];
+	private ArrayList<Book> books;
+	public BookShelf() {
+		this.books = new ArrayList<Book>();
 	}
 	public Book getBookAt(int index) {
-		return books[index];
+		return books.get(index);
 	}
 	public void appendBook(Book book) {
-		this.books[last] = book;
-		last++;
+		this.books.add(book);
 	}
 	public int getLength() {
-		return last;
+		return books.size();
 	}
 	public Iterator iterator() {
 		return new BookShelfIterator(this);
--- src/iterator/bookshelf/Main.java
+++ src/iterator/bookshelf/Main.java
@@ -5,7 +5,7 @@ import iterator.Iterator;
 public class Main {
 
 	public static void main(String[] args) {
-		BookShelf bookShelf = new BookShelf(4);
+		BookShelf bookShelf = new BookShelf();
 		bookShelf.appendBook(new Book("Book A"));
 		bookShelf.appendBook(new Book("Book B"));
 		bookShelf.appendBook(new Book("Book C"));
