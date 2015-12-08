a72daa03eda0f7929ccb8b993859e907996c147a
Tomohisa Kuranari <m.qureana@gmail.com>
2015-12-09 00:52:27 +0900

    例題追加

--- src/factory_method/idcard/IDCard.java
+++ src/factory_method/idcard/IDCard.java
@@ -3,14 +3,19 @@ import factory_method.framework.*;
 
 public class IDCard extends Product {
 	private String owner;
-	IDCard(String owner) {
+	private int id;
+	IDCard(String owner, int id) {
 		System.out.println(owner + "のカードを作ります。");
 		this.owner = owner;
+		this.id = id;
 	}
 	public void use() {
 		System.out.println(owner + "のカードを使います。");
 	}
 	public String getOwner() {
 		return owner;
+	}
+	public int getId(){
+		return id;
 	}
 }
--- src/factory_method/idcard/IDCardFactory.java
+++ src/factory_method/idcard/IDCardFactory.java
@@ -5,14 +5,24 @@ import factory_method.framework.*;
 
 public class IDCardFactory extends Factory{
 	private List owners = new ArrayList();
+	private int index = 0;
+	private Map ownerTable = new HashMap();
 	protected Product createProduct(String owner) {
-		return new IDCard(owner);
+		return new IDCard(owner, index++);
 	}
+
 	protected void registerProduct(Product product) {
-		owners.add(((IDCard)product).getOwner());
+		IDCard idCard = (IDCard)product;
+		owners.add(idCard.getOwner());
+		ownerTable.put(idCard.getId(), idCard.getOwner());
 	}
+
 	public List getOwners() {
 		return owners;
+	}
+	
+	public Map getOwnerTable(){
+		return ownerTable;
 	}
 	
 }
